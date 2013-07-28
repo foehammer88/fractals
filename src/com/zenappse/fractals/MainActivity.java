90446adc80ef084e3e2d3564b8ee7478fe54a187
mas7967 <mark.schillaci@gmail.com>
2013-07-28 03:21:28 -0400

    Add basic framework.
    May not be functioning.

--- src/com/zenappse/fractals/MainActivity.java
+++ src/com/zenappse/fractals/MainActivity.java
@@ -3,13 +3,27 @@ package com.zenappse.fractals;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends Activity {
+
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private float x, y, z;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        createGraph();
 	}
 
 	@Override
@@ -18,5 +32,31 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+    public void createGraph() {
+        bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        x = 10;
+        y = 10;
+        onDraw(canvas);
+    }
+
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, x, y, null);
+    }
+
+    public int getBitmapWidth() {
+        if(bitmap != null) {
+            return bitmap.getWidth();
+        }
+        return 0;
+    }
+
+    public int getBitmapHeight() {
+        if(bitmap != null) {
+            return bitmap.getHeight();
+        }
+        return 0;
+    }
 
 }
