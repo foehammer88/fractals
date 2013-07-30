3bc7946e5815c234645abc05e6b3e1780d587959
mas7967 <mark.schillaci@gmail.com>
2013-07-30 00:19:17 -0400

    Created basic proof of concept.
    Very minimal, shows sort of the idea.

--- src/com/zenappse/fractals/MainActivity.java
+++ src/com/zenappse/fractals/MainActivity.java
@@ -1,64 +1,100 @@
 package com.zenappse.fractals;
 
-import android.os.Bundle;
+/**
+ * Created by Mark on 7/29/13.
+ */
 import android.app.Activity;
-import android.view.Menu;
-import android.view.Window;
-import android.view.WindowManager;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+import java.lang.Math.*;
 
 public class MainActivity extends Activity {
 
-    private Bitmap bitmap;
-    private Canvas canvas;
-    private float x, y, z;
+    Bitmap bitmap;
+    Button button;
+    ImageView image;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        createGraph();
-	}
+        addListenerOnButton();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-    public void createGraph() {
-    	int[] colors = new int[10000];
-    	for (int i = 0; i<10000; i++){
-    		colors[i] = Color.RED;
-    	}
-        bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bitmap);
-        x = 10;
-        y = 10;
-        canvas.drawColor(Color.WHITE);
-        canvas.drawRGB(255, 0, 0);
     }
 
-    public int getBitmapWidth() {
-        if(bitmap != null) {
-            return bitmap.getWidth();
-        }
-        return 0;
-    }
+    public void addListenerOnButton() {
 
-    public int getBitmapHeight() {
-        if(bitmap != null) {
-            return bitmap.getHeight();
-        }
-        return 0;
+        image = (ImageView) findViewById(R.id.FractalView1);
+
+        button = (Button) findViewById(R.id.buttonChangeParam);
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Our window bounds for now, and our number of pixels
+                double xmin = -1.5;
+                double xmax = 1.5;
+                double ymin = -1.5;
+                double ymax = 1.5;
+
+                int xpix = 200;
+                int ypix = 200;
+
+                // Create a random number generator and a value for parameter 'c'
+                Random rand = new Random();
+                double creal = .8*rand.nextDouble()-.4;
+                double cimag = .8*rand.nextDouble()-.4;
+
+                // Initiate colors, cutoff, various extras and begin our loop
+                int[] colors = new int[xpix*ypix];
+                double cutoff = 500000;
+                int maxiters = 255;
+                int pixel_count = 0;
+                double x0, y0, zreal, zimag, ztmp, curnorm;
+                for (int a = 1; a<=xpix; a++){
+                    for (int b = 1; b<=ypix; b++){
+                        x0 = ((xmax-xmin)/xpix)*a - xmax;
+                        y0 = ((ymax-ymin)/ypix)*b - ymax;
+                        zreal = x0;
+                        zimag = y0;
+                        curnorm = Math.sqrt(((zreal*zreal)+(zimag*zimag)));
+
+                        int curcount = 1;
+                        while( (curcount < maxiters) && (curnorm < cutoff) ){
+                            ztmp = zreal;
+                            zreal = zreal*zreal - zimag*zimag + creal;
+                            zimag = 2*ztmp*zimag + cimag;
+
+                            curnorm = Math.sqrt(((zreal*zreal)+(zimag*zimag)));
+                            curcount++;
+                        }
+                        colors[pixel_count] = Color.rgb(curcount, 0, 0);
+                        pixel_count++;
+                    }
+                }
+                bitmap = Bitmap.createBitmap(colors, xpix, ypix, Bitmap.Config.ARGB_8888);
+                image.setImageBitmap(bitmap);
+
+                String str1 = new DecimalFormat("#.####").format(creal);
+                String str2 = new DecimalFormat("#.####").format(cimag);
+
+                TextView tv= (TextView) findViewById(R.id.currentParam);
+                tv.setText("Current 'c' value: c = " + str1 + " + " + str2 + "*i");
+            }
+
+        });
+
     }
 
 }
